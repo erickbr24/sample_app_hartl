fe1052711fa69ea60192f30c6b75c175d6ce9e51
Erick <erickbr24@gmail.com>
2017-07-21 22:13:59 -0400

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test 'should not sign up user' do 
+  	get signup_path
+  	assert_no_difference 'User.count' do 
+  		post signup_path, params: { user: { name: "", email: "user@invaid", password: "foo", password_confirmation: "bar"}}
+  	end
+  	assert_template 'users/new'
+  	assert_select "div", "The form contains 4 errors" #assert_select 'div.error_explanation' also works
+  end
+
+  test 'should sign up user' do 
+  	get signup_path 
+  	assert_difference 'User.count', 1 do 
+  		post signup_path, params: {user: {name: "Erickio", email: "erickio@io.com", password: "blablabla", password_confirmation: "blablabla"}}
+  	end
+  	follow_redirect!
+  	assert_template 'users/show'
+  	assert_not flash.empty?
+  end
+end
